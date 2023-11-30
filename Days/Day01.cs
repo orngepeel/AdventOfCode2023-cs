d23c2f5f28418f503132c404a9f1b89b9414ae1f
Devon Braner <68525598+orngepeel@users.noreply.github.com>
2023-11-30 18:15:32 -0500

    Add project files.

---
+++ Days/Day01.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Days
+{
+    internal class Day01
+    {
+        public Day01() 
+        {
+            Console.WriteLine("Day 1!\n");
+            Console.WriteLine(Part1() + "\n");
+            Console.WriteLine(Part2() + "\n");
+        }
+
+        private String Part1()
+        {
+            return "";
+        }
+
+        private String Part2() 
+        {
+            return "";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using AdventOfCode2023.Days;
+namespace AdventOfCode2023;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        _ = new Day01();
+    }
+}

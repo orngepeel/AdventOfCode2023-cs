c22c8b80894a05fd31b523b0849ddda98ce3264e
Devon Braner <68525598+orngepeel@users.noreply.github.com>
2023-12-04 16:11:16 -0500

    day 4

--- Days/Day04.cs
+++ Days/Day04.cs
@@ -16,14 +16,38 @@ namespace AdventOfCode2023.Days
 
         private static string Day04Part1()
         {
-            StreamReader input = new("..\\..\\..\\Inputs\\Day04_Part1.txt");
+            StreamReader input = new("..\\..\\..\\Inputs\\Day04.txt");
             int runningTotal = 0;
             
             string line = input.ReadLine()!;
 
             while (line != null)
             {
-                string[] subs = line.Split(':', ';');
+                string[] subs = line.Split(':', '|');
+                string[] winningNums = subs[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string[] gameNums = subs[2].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                int gameScore = 0;
+
+                Dictionary<string, bool> map = new();
+
+                for (int i = 0; i < winningNums.Length; i++)
+                {
+                    map.Add(winningNums[i], true);
+                }
+
+                for (int i = 0; i < gameNums.Length; i++)
+                {
+                    if (map.ContainsKey(gameNums[i]))
+                    {
+                        if (gameScore == 0)
+                            gameScore++;
+                        else
+                            gameScore *= 2;
+                    }
+                }
+
+                runningTotal += gameScore;
 
                 line = input.ReadLine()!;
             }
@@ -34,15 +58,54 @@ namespace AdventOfCode2023.Days
 
         private static string Day04Part2()
         {
-            StreamReader input = new StreamReader("..\\..\\..\\Inputs\\Day04_Part1.txt");
+            StreamReader input = new("..\\..\\..\\Inputs\\Day04.txt");
             int runningTotal = 0;
 
             string line = input.ReadLine()!;
 
+            Dictionary<int, int> cardInstances = new();
+
             while (line != null)
             {
-                string[] subs = line.Split(':', ';');
-                
+                string[] subs = line.Split(':', '|');
+                int gameNum = Int32.Parse(subs[0].Split(' ', StringSplitOptions.RemoveEmptyEntries)[1]);
+                string[] winningNums = subs[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string[] gameNums = subs[2].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                int matches = 0;
+                if (cardInstances.ContainsKey(gameNum))
+                    cardInstances[gameNum] += 1;
+                else
+                    cardInstances.Add(gameNum, 1);
+
+                runningTotal += cardInstances[gameNum];
+
+                Dictionary<string, bool> map = new();
+
+                for (int i = 0; i < winningNums.Length; i++)
+                {
+                    map.Add(winningNums[i], true);
+                }
+
+                for (int i = 0; i < gameNums.Length; i++)
+                {
+                    if (map.ContainsKey(gameNums[i]))
+                    {
+                        matches++;
+                    }
+                }
+
+                for (int i = 0; i < cardInstances[gameNum]; i++)
+                {
+                    for (int j = 1; j <= matches; j++)
+                    {
+                        if (cardInstances.ContainsKey(gameNum + j))
+                            cardInstances[gameNum + j] += 1;
+                        else
+                            cardInstances.Add(gameNum + j, 1);
+                    }
+                }
+
                 line = input.ReadLine()!;
             }
 

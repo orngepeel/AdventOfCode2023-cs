d0611a530cb91b160649d6ec82f7bd41096ad3f9
Devon Braner <68525598+orngepeel@users.noreply.github.com>
2023-12-08 03:49:42 -0500

    Day 8 setup

---
+++ Days/Day08.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.Days
+{
+    internal class Day08
+    {
+        public Day08()
+        {
+            Console.WriteLine("    Part 1 Solution: {0}\n", Day08Part1());
+            Console.WriteLine("    Part 2 Solution: {0}\n", Day08Part2());
+        }
+
+
+        private static string Day08Part1()
+        {
+            StreamReader input = new("..\\..\\..\\Inputs\\Day08.txt");
+
+            return "";
+        }
+
+        private static string Day08Part2()
+        {
+            StreamReader input = new("..\\..\\..\\Inputs\\Day08.txt");
+
+            return "";
+        }
+    }
+}
